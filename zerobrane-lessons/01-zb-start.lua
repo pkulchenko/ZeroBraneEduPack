4d295dea933c7a5ca69f2d01e14fa8098e0dc585
Paul Kulchenko <paul@kulchenko.com>
2012-02-27 20:39:09 -0800

    Added zerobrane studio lessons

---
+++ zerobrane-lessons/01-zb-start.lua
@@ -0,0 +1,10 @@
+--[[ ~previous;00-contents.lua~ | ~contents;00-contents.lua~ | ~next;02-controls.lua~
+
+! Hi there!
+
+Welcome to the ZeroBrane Studio tour. ZeroBrane Studio is an application that provides you with everything you need to create and run your own programs. 
+
+In about 15 minutes you will know enough to create, save, and run programs in this environment. It will take longer to figure out *what* you want your programs to do and *how* exactly you should write them to do that, but fear not! Our team is working on more tutorials to help you learn programming, graphics, and other topics you may find interesting.
+
+Click the ~next;02-controls.lua~ link at the top of the screen to get started. 
+]]
---
+++ zerobrane-lessons/09-adjustment.lua
@@ -0,0 +1,10 @@
+--[[ ~previous;08-toolbar.lua~ | ~contents;00-contents.lua~ | ~next;10-done.lua~
+
+! Personalizing the environment
+
+The default settings and layout should work for most users, but occasionally you may want to have more space available on the screen or prefer to have a window moved to a different position.
+
+You can close @the Project@ and @Output/Console@ windows if you are not using them to get more screen space. You can switch back to the default by using @View | Default Layout@.
+
+You can *resize* the windows by dragging the splitters between them. You can also *move* the windows by clicking and holding the mouse on the window caption area and dragging the window to a new location. You can release the mouse when one side of the main window changes its color to light blue. The new configuration will then be used until you reset or change it.
+]]
