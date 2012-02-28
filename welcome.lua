f1b89227f9cd5eecbc56101b13223645345c3ef4
Paul Kulchenko <paul@kulchenko.com>
2012-02-27 20:41:04 -0800

    Added initial batch of programming lessons

---
+++ welcome.lua
@@ -0,0 +1,14 @@
+--[[
+
+! Welcome!
+
+ZeroBrane Studio is a *simple* and *friendly* environment to learn and explore programming, graphics, games, and other topics. Start with the lessons or go straight to the demonstrations to see what you can create.
+
+!Lessons
+- ~ZeroBrane Studio basics;+zerobrane-lessons/01-zb-start.lua~
+- ~Programming basics;+programming-lessons/01-progr-start.lua~
+
+!Demos
+- ~Turtle graphics demo;+turtle-samples/demo.lua~
+- ~Spirograph graphics demo;+spirograph-samples/demo.lua~
+]]
