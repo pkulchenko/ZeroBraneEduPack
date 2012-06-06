51fb9003c4092b1d44af9c0d75c2d31c157ee6e6
Paul Kulchenko <paul@kulchenko.com>
2012-06-06 14:24:15 -0700

    Fixed links to open in a new window

--- welcome.lua
+++ welcome.lua
@@ -5,8 +5,8 @@
 ZeroBrane Studio is a **simple** and **friendly** environment to learn and explore programming, graphics, games, and other topics. Start with the lessons or go straight to the demonstrations to see what you can create.
 
 #Lessons
-- [ZeroBrane Studio basics](zerobrane-lessons/01-zb-start.lua)
-- [Programming basics](programming-lessons/01-progr-start.lua)
+- [ZeroBrane Studio basics](+zerobrane-lessons/01-zb-start.lua)
+- [Programming basics](+programming-lessons/01-progr-start.lua)
 
 #Demos
 - [Turtle graphics demo](+turtle-samples/demo.lua)
