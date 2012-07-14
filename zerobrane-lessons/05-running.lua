d37f9d46c4a1b2d8a07f619c958797e13061c880
Paul Kulchenko <paul@kulchenko.com>
2012-07-13 22:40:42 -0700

    Updated an example in zerobrane lessons

--- zerobrane-lessons/05-running.lua
+++ zerobrane-lessons/05-running.lua
@@ -4,8 +4,13 @@
 
 It would not be that interesting if you could only edit your programs. You want your programs to **do** something, don't you? You can do that by **running** (or **executing**) your program. 
 
-The page you are reading right now is a script that you can execute. Press `F6` or go to `Project | Run` menu at the top of the window to run it. You should see a new window open with a rounded square being drawn.
+The page you are reading right now is a script that you can execute. Press `F6` or go to `Project | Run` menu at the top of the window to run it. It will calculate and print a square root of 612.
 ]]
 
-require "spirograph"
-spiro(60, -45, -80, 360, 0.002, true)
+function sqrt(x)
+  local function good(guess) return math.abs((guess^2) - x) < 1e-5 and guess end
+  local function better(guess) return (guess + x / guess) / 2 end
+  local function try(guess) return good(guess) or try(better(guess)) end
+  return try(10)
+end
+print(sqrt(612))
