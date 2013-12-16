3a40212e7ae97fede5277285a8b973a7c9d61e19
Paul Kulchenko <paul@kulchenko.com>
2013-12-16 11:30:53 -0800

    Updated love2d example configuration for 0.9.0 API changes.

--- love2d-samples/trajectory/conf.lua
+++ love2d-samples/trajectory/conf.lua
@@ -1,5 +1,6 @@
 function love.conf(c)
   c.title = "Trajectory"
-  c.screen.width = 1024
-  c.screen.height = 660
+  local window = c.screen or c.window -- love 0.9 renamed "screen" to "window"
+  window.width = 1024
+  window.height = 660
 end
