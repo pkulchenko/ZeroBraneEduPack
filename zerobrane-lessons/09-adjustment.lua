31bfdbfd58227734015d6c4736d4f1739b77d022
Paul Kulchenko <paul@kulchenko.com>
2014-06-18 19:31:27 -0700

    Updated zerobrane lessons on moving windows.

--- zerobrane-lessons/09-adjustment.lua
+++ zerobrane-lessons/09-adjustment.lua
@@ -4,7 +4,7 @@
 
 The default settings and layout should work for most users, but occasionally you may want to have more space available on the screen or prefer to have a window moved to a different position.
 
-You can **close** the `Project` and `Output/Console` windows if you are not using them to get more screen space. You can switch back to the default by using `View | Default Layout`.
+You can **close** and **open** the `Project` and `Output/Console` windows by going to the `View` menu and selecting the window to toggle from the menu. You can also switch back to the default layout by using `View | Default Layout`.
 
-You can **resize** the windows by dragging the splitters between them. You can also **move** the windows by clicking and holding the mouse on the window caption area and dragging the window to a new location. You can release the mouse when one side of the main window changes its color to light blue. The new configuration will then be used until you reset or change it.
+You can **resize** windows by dragging the splitters between them. You can also **move** the windows by double clicking on their tab background, clicking and holding the mouse on the window caption area, and dragging the window to a new location. You can release the mouse when one side of the main window changes its color to light blue. The new configuration will then be used until you reset or change it.
 ]]
