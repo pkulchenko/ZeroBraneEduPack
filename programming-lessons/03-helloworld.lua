e24af32604a1e9003e432e0010c69808d6fc1f2d
Paul Kulchenko <paul@kulchenko.com>
2014-06-18 19:30:41 -0700

    Updated programming lessons.

--- programming-lessons/03-helloworld.lua
+++ programming-lessons/03-helloworld.lua
@@ -6,7 +6,7 @@ The very first program all future programmers create is called "Hello, World!". 
 
 The text that you see below is the actual script that does that; [click here](macro:run), press `F6` on the keyboard, or click on `Project | Run` menu at the top to run the script. You should see `Hello, World!` printed in the `Output` section below. **Try it.**
 
-Congratulations! You have made your first step as a computer programmer! Now modify the text and see how it works. You can type anything like "Hi, Andrey!" or "I did it". Do not forget to run the script again after you change it.
+Congratulations! You have made your first step as a computer programmer! Now modify the text and see how it works. You can type anything like "Hi, Andrey!" or "I did it". Do not forget to run the script again after changing it.
 ]]
 
 print("Hello, World!")
