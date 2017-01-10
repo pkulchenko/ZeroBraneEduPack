29b65598cdeb9e4f69476d419e378abf2d4cc82d
Paul Kulchenko <paul@kulchenko.com>
2017-01-10 13:51:52 -0800

    Fixed decimal formatting for Lua 5.3 (closes pkulchenko/ZeroBraneStudio#722).

--- programming-lessons/13-format.lua
+++ programming-lessons/13-format.lua
@@ -6,7 +6,7 @@ If you look at the output of your script, you may not like the long number that 
 
 In this case we will **format** the string. Every string may include placeholders and provides a special way (`format` method) to put values in those placeholders. Placeholders start with `%` (percent sign). Here is a way to include a number: `print(("I'm %d years old"):format(8))`. Try it in the console. Type it; don't copy!
 
-We can now format our message using the number placeholder (`%d`).
+We can now format our message using the number placeholders: `%d` for integers (like `2200)` and `%f` for decimals (like `1.5`).
 ]]
 
-print(('A bulb rated for 2200 hours will last %d years'):format(2200/6/365))
+print(('A bulb rated for %d hours will last about %.1f years'):format(2200, 2200/6/365))
