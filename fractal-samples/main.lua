7baa58eb20202d7718fb0f40a3394a2d5569237b
Deyan Dobromirov <dvd_video@abv.bg>
2017-10-24 11:22:44 +0300

    Updated: Fractal sample using libraries as function tables

--- fractal-samples/main.lua
+++ fractal-samples/main.lua
@@ -1,8 +1,9 @@
 require("turtle")
 require("wx")
-require("complex")
-require("fractal")
-require("colormap")
+
+local compl = require("complex")
+local fract = require("fractal")
+local clmap = require("colormap")
 
 -- z(0) = z,    z(n+1) = z(n)*z(n) + z,    n=0,1,2, ...    (1)
 
@@ -17,17 +18,18 @@ local szIm  = 2
 local nStep = 35
 local nZoom = 15
 local iTer  = 60
-local sfrac = "julia6"
+local sfrac = "mandelbrot"
 local spale = "wikipedia"
 local brdcl = nil -- colr(0, 250, 100)
 local brdup = nil -- true
 
 --- Dinamic parameters and constants
-local cexp   = ToComplex(math.exp(1))
+local cexp   = compl.New(math.exp(1))
 local w2, h2 = W/2, H/2
 local gr     = 1.681
 
-setColorMap("wikipedia",
+-- https://upload.wikimedia.org/wikipedia/commons/b/b3/Mandel_zoom_07_satellite.jpg
+clmap.setColorMap("wikipedia",
 {
   { 66,  30,  15}, -- brown 3
   { 25,   7,  26}, -- dark violett
@@ -58,28 +60,41 @@ Cent: {-0.10109678819444,-0.95628602430556}
 Area: {-0.10109910300926,-0.10109447337963,-0.95628833912037,-0.95628370949074}
 ]]
 
-local S = makeFractal(W,H,-szRe,szRe,-szIm,szIm,brdcl,brdup)
+local S = fract.New("z-plane",W,H,-szRe,szRe,-szIm,szIm,brdcl,brdup)
       S:SetControlWX(wx)
    --    S:SetArea(-1.406574048011,-1.406574042524,0.00025352709190672,0.00025353257887517)
-      S:Register("FUNCT","mandelbrot",function (Z, C, R) Z:Pow(2); Z:Add(C); R[1] = Z:getAngRad(); end )
-      S:Register("FUNCT","mandelbar" ,function (Z, C, R) Z:Pow(2); Z:NegIm(); Z:Add(C) end )
-      S:Register("FUNCT","julia1"    ,function (Z, C, R) Z:Pow(2); Z:Add(ToComplex("-0.8+0.156i")) end )
-      S:Register("FUNCT","julia2"    ,function (Z, C, R) Z:Set(cexp^(Z^3) - 0.621) end )
-      S:Register("FUNCT","julia3"    ,function (Z, C, R) Z:Set(cexp^Z) Z:Sub(0.65) end )
-      S:Register("FUNCT","julia4"    ,function (Z, C, R) Z:Pow(3) Z:Add(0.4)  end )
-      S:Register("FUNCT","julia5"    ,function (Z, C, R) Z:Set((Z^4) * cexp^Z + 0.41 )  end )
-      S:Register("FUNCT","julia6"    ,function (Z, C, R) Z:Set((Z^3) * cexp^Z + 0.33 )  end )
+      S:Register("FUNCT","mandelbrot",
+        function (Z, C, R) Z:Pow(2); Z:Add(C); R[1] = Z:getAngRad(); end )
+      S:Register("FUNCT","mandelbar",
+        function (Z, C, R) Z:Pow(2); Z:NegIm(); Z:Add(C) end )
+      S:Register("FUNCT","julia1",
+        function (Z, C, R) Z:Pow(2); Z:Add(ToComplex("-0.8+0.156i")) end )
+      S:Register("FUNCT","julia2",
+        function (Z, C, R) Z:Set(cexp^(Z^3) - 0.621) end )
+      S:Register("FUNCT","julia3",
+        function (Z, C, R) Z:Set(cexp^Z) Z:Sub(0.65) end )
+      S:Register("FUNCT","julia4",
+        function (Z, C, R) Z:Pow(3) Z:Add(0.4)  end )
+      S:Register("FUNCT","julia5",
+        function (Z, C, R) Z:Set((Z^4) * cexp^Z + 0.41 )  end )
+      S:Register("FUNCT","julia6",
+        function (Z, C, R) Z:Set((Z^3) * cexp^Z + 0.33 )  end )
       S:Register("PALET","default"   ,function (Z, C, i) return
         (math.floor((64  * i) % maxCl)), (math.floor((128 * i) % maxCl)), (math.floor((192 * i) % maxCl)) end )
-      S:Register("PALET","rediter"   ,function (Z, C, i) return math.floor((1-(i / iTer)) * maxCl), 0, 0 end )
-      S:Register("PALET","greenbl"   ,function (Z, C, i, x, y)
-        local it = i / iTer; return math.floor(0), math.floor((1 - it) * maxCl), math.floor(it * maxCl) end)
-      S:Register("PALET","wikipedia"   ,function (Z, C, i, x, y, R) return getColorMap("wikipedia",i) end)
-      S:Register("PALET","region"      ,function (Z, C, i, x, y) return getColorRegion(i,iTer,10) end)
-      S:Register("PALET","hsl", function (Z, C, i, x, y) local it = i / iTer; return getColorHSL(it*360,it,it) end)
-      S:Register("PALET","hsv", function (Z, C, i, x, y) local it = i / iTer; return getColorHSV(it*360,1,1) end)
-      S:Register("PALET","wikipedia_r"   ,function (Z, C, i, x, y, R) return getColorMap("wikipedia",i * (R[1] and 1+math.floor(math.abs(R[1])) or 1)) end)
-
+      S:Register("PALET","rediter",
+        function (Z, C, i) return math.floor((1-(i / iTer)) * maxCl), 0, 0 end )
+      S:Register("PALET","greenbl",
+        function (Z, C, i, x, y) local it = i / iTer; return math.floor(0), math.floor((1 - it) * maxCl), math.floor(it * maxCl) end)
+      S:Register("PALET","wikipedia",
+        function (Z, C, i, x, y, R) return clmap.getColorMap("wikipedia",i) end)
+      S:Register("PALET","region",
+        function (Z, C, i, x, y) return clmap.getColorRegion(i,iTer,10) end)
+      S:Register("PALET","hsl",
+        function (Z, C, i, x, y) local it = i / iTer; return getColorHSL(it*360,it,it) end)
+      S:Register("PALET","hsv",
+        function (Z, C, i, x, y) local it = i / iTer; return getColorHSV(it*360,1,1) end)
+      S:Register("PALET","wikipedia_r",function (Z, C, i, x, y, R)
+        return clmap.getColorMap("wikipedia",i * (R[1] and 1+math.floor(math.abs(R[1])) or 1)) end)
 
 S:Draw(sfrac,spale,iTer)
 
@@ -105,6 +120,4 @@ while true do
     elseif(key == S:GetKey("dirR")) then S:MoveCenter( nStep,0) end
     S:Draw(sfrac,spale,iTer)
   end
-  -- updt()
-  --wait(0.2)
 end
