444d926ed31ecbc491cc7b3899d9ea2378a09a23
Paul Kulchenko <paul@kulchenko.com>
2017-11-02 17:47:48 -0700

    Updated to use `wait` function provided in the `turtle` library (#6).

--- fractal-samples/ytree.lua
+++ fractal-samples/ytree.lua
@@ -1,14 +1,6 @@
 require("turtle")
 local fractal = require("fractal")
 
-local function waitSeconds(Add)
--- NOTE: SYSTEM-DEPENDENT, adjust as necessary
-  if(Add > 0) then
-    local i=os.clock() + Add
-    while(os.clock() < i) do end
-  end
-end
-
 local D, W, H = 8, 1000,1000
 local oTree = fractal.New("ytree", D, colr(100, 50, 255))
 if(oTree) then
@@ -17,5 +9,5 @@ if(oTree) then
   zero(0, 0)
   updt(true) -- disable auto updates
   oTree:Allocate(oTree) 
-  oTree:Draw(oTree,W/2,0,W/4,H/4,waitSeconds,0.01)
+  oTree:Draw(oTree,W/2,0,W/4,H/4,wait,0.01)
 end
