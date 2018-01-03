6c74aa1249029ebfa158b15a9c86a4e17567586a
Deyan Dobromirov <dvd_video@abv.bg>
2018-01-03 09:24:29 +0200

    Added: Using local real and imaginary part instead of calling the method 3 times in a iteration

--- complex-samples/demo.lua
+++ complex-samples/demo.lua
@@ -1,5 +1,5 @@
 require("turtle")
-local drw = require("chartmap")
+local crt = require("chartmap")
 local cmp = require("complex")
 local col = require("colormap")
 
@@ -12,25 +12,28 @@ size(W,H)
 zero(0, 0)
 updt(false) -- disable auto updates
 
+local drwText = true
+
 local V0 = cmp.New(200,200):Print("Initial velocity: ","\n")
 local P  = cmp.Convert("0+j0"):Print("Position        : ","\n")
 local G  = cmp.Convert({0,-9.8}):Print("Gravity         : ","\n")
 local V  = cmp.Convert(V0):Print("Moment velocity : ","\n")
-local minX, maxX, maxY, minY, traJ = 0, 0, 0, 0, {cmp.New(P)}
+local minX, maxX, minY, maxY, traJ = 0, 0, 0, 0, {cmp.New(P)}
 
 while(P:getImag() >= 0) do
   V:Add(G); P:Add(V)
   traJ[#traJ+1] = P:getDupe()
-  if(P:getReal() >= maxX) then maxX = P:getReal() end
-  if(P:getImag() >= maxY) then maxY = P:getImag() end
-  if(P:getReal() <= minX) then minX = P:getReal() end
-  if(P:getImag() <= minY) then minY = P:getImag() end
+  local xP, yP = P:getReal(), P:getImag()
+  if(xP >= maxX) then maxX = xP end
+  if(yP >= maxY) then maxY = yP end
+  if(xP <= minX) then minX = xP end
+  if(yP <= minY) then minY = yP end
 end
 
 local clGrn = colr(col.getColorGreenRGB())
-local intX  = drw.newInterval("WinX", minX, maxX, 0, W)
-local intY  = drw.newInterval("WinY", minY, maxY, H, 0)
-local trAj  = drw.newTracer("Trajectory"):setInterval(intX, intY)
+local intX  = crt.newInterval("WinX", minX, maxX, 0, W)
+local intY  = crt.newInterval("WinY", minY, maxY, H, 0)
+local trAj  = crt.newTracer("Trajectory"):setInterval(intX, intY)
 local zEro  = intY:Convert(0):getValue()
 
 line(0, zEro, W, zEro)
@@ -40,6 +43,7 @@ for ID = 1, #traJ do
   local cPos = traJ[ID]
   local Re, Im = cPos:getReal(), cPos:getImag()
   trAj:putValue(Re, Im):Draw(clGrn)
-  text(tostring(cPos),0,intX:Convert(Re):getValue(),intY:Convert(Im):getValue())
+  if(drwText) then
+    text(tostring(cPos),0,intX:Convert(Re):getValue(),intY:Convert(Im):getValue()) end
   updt()
 end
