99ab2a840e3ead9db058a1295dfb4bbc3d5f4323
Deyan Dobromirov <dvd_video@abv.bg>
2018-09-26 11:36:51 +0300

    Fixed: Complex demo infinite loop when projectile does not fall on the ground

--- complex-samples/demo.lua
+++ complex-samples/demo.lua
@@ -14,20 +14,23 @@ updt(false) -- disable auto updates
 
 local drgCoef = 2
 local nMarg   = 30
+local nMaxItr = 1000
+local nTimeFR = 0.1
 local drwText = true
 local V0 = cmp.getNew(200,200):Print("Initial velocity : ","\n")
 local P  = cmp.convNew("0+j0"):Print("Position         : ","\n")
 local G  = cmp.convNew({0,-9.8}):Print("Gravity          : ","\n")
 local V  = cmp.convNew(V0):Print("Moment velocity  : ","\n")
-local Wv = cmp.convNew(-4,2):Print("Wind velocity    : ","\n")
+local Wv = cmp.convNew(-3,3):Print("Wind velocity    : ","\n")
 local minX, maxX, minY, maxY, traJ = 0, 0, 0, 0, {cmp.getNew(P)}
 
-while(P:getImag() >= 0) do
+for ID = 1, nMaxItr do
+  if(P:getImag() < 0) then break end
   V:Add(G)                         -- Apply gravity
   V:Add(V:getUnit():Mul(-drgCoef)) -- Calculate drag
-  V:Add(Wv)                        -- Add wind velocity
+  V:Add(Wv)                        -- Apply wind
   P:Add(V)                         -- Calcilate position
-  traJ[#traJ+1] = P:getNew()
+  traJ[ID] = P:getNew()
   local xP, yP = P:getParts()
   if(xP >= maxX) then maxX = xP end
   if(yP >= maxY) then maxY = yP end
@@ -37,14 +40,14 @@ end
 
 local oX,aN = cmp.getNew(1), 0
 local clGrn = colr(col.getColorGreenRGB())
-local intX  = crt.New("interval","WinX", minX, maxX,   nMarg, W-nMarg)
-local intY  = crt.New("interval","WinY", minY, maxY, H-2*nMarg,   0)
+local intX  = crt.New("interval","WinX", minX, maxX, nMarg, W-nMarg)
+local intY  = crt.New("interval","WinY", minY, maxY, H-2*nMarg, 0)
 local trAj  = crt.New("tracer","Trajectory"):setInterval(intX, intY)
 local zEro  = intY:Convert(0):getValue()
 
 line(0, zEro, W, zEro)
 
-for ID = 1, #traJ do wait(0.05)
+for ID = 1, #traJ do wait(nTimeFR)
   local cPos = traJ[ID]
   if(traJ[ID+1]) then
     aN = (traJ[ID+1] - traJ[ID]):getAngDegVec(oX)-90 end
