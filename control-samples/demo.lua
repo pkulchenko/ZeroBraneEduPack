0b2cd3bb51465397db9b364e8af7fe39ec8ddea0
dvdvideo1234 <dvd_video@abv.bg>
2019-05-07 11:33:23 +0300

    Added: Grid to the PID loop sample controlled step response

--- control-samples/demo.lua
+++ control-samples/demo.lua
@@ -8,31 +8,35 @@ io.stdout:setvbuf("no")
 
 -- Manual part
 local W, H       = 1200, 800 -- window dimensions
-local minC, maxC = -20, 20   -- control limists
-local stVal      = 10        -- Step responce final value               
+local minC, maxC = -25, 25   -- control limists
+local stVal      = 15        -- Step responce final value               
 local To         = 0.05      -- Sampling time
 local wAit       = 0.01      -- Simulation speed-up
-local tScale     = 50        -- End time relative to the sampling time
+local tScale     = 30        -- End time relative to the sampling time
 -- local tFixed     = 5      -- Fixed time
 -- Automated part
-local endTm = tFixed and tFixed or (tScale * To)
-local intX  = crt.New("interval","WinX",  0,endTm, 0, W)
-local intY  = crt.New("interval","WinY", -1, 15 , H, 0)
-local APR   = sig.New("plant",To,{8},{7 , 3, 1},"Hesitating plant"):Dump()
-local PID   = sig.New("control",To,"Lin-QR"):Setup({0.051, 0.009, 6, minC, maxC}):setStruct(false,false):Dump()
-local trRef = crt.New("tracer","Ref"):setInterval(intX, intY)
-local trCon = crt.New("tracer","Con"):setInterval(intX, intY)
-local trPV  = crt.New("tracer","PV" ):setInterval(intX, intY)
-local function form(nV) return ("%03.2f"):format(nV) end
-open("Trasition processes")
-size(W,H)
-zero(0, 0)
-updt(false) -- disable auto updates
 local curTm, yTx, dyTx = 0, 0, 15
 local clRed = colr(col.getColorRedRGB())
 local clGrn = colr(col.getColorGreenRGB())
 local clBlu = colr(col.getColorBlueRGB())
 local clBlk = colr(col.getColorBlackRGB())
+local endTm = tFixed and tFixed or (tScale * To)
+local intX  = crt.New("interval","WinX",  0,endTm, 0, W)
+local intY  = crt.New("interval","WinY", -1, maxC , H, 0)
+local APR   = sig.New("plant",To,{8},{7 , 3, 1},"Hesitating plant"):Dump()
+local PID   = sig.New("control",To,"Lin-QR"):Setup({0.051, 0.009, 6, minC, maxC}):setStruct(false,false):Dump()
+local trRef = crt.New("tracer","Ref"):setInterval(intX, intY)
+local trCon = crt.New("tracer","Con"):setInterval(intX, intY)
+local trPV  = crt.New("tracer","PV" ):setInterval(intX, intY)
+local scOpe = crt.New("scope"):setInterval(intX, intY)
+      scOpe:setSize(W, H):setColor(clBlk, clGry):setBorder():setDelta(To, 1)
+local function form(nV) return ("%03.2f"):format(nV) end
+open("Trasition processes")
+size(W,H)
+zero(0, 0)
+updt(false) -- disable auto updates
+
+scOpe:Draw(false, false, true)
 
 text("( Blue ) System reference", 0, 0, yTx); yTx = yTx + dyTx
 text("( Green ) Control signal", 0, 0, yTx); yTx = yTx + dyTx
